289d1e52471ef4deb7793e59c36f4a832915a634
KAKUL GUPTA <Kakul_Gupta@external.mckinsey.com>
2016-10-29 23:44:11 +0530

    init fixes

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
+import angular from "angular";
 import 'angular-ui-router';
 import routeConfig from './app.router.js';
-import angular from "angular";
 import MainCtrl from "./controllers/main.controller.js";
 // import CacheService from "services/cache.service.js";
 
